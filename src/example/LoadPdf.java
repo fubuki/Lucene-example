e19dcb71ab1c945b08a34aee54b5771bab57af1a
fubuki <fubuki@users.noreply.github.com>
2014-03-24 22:24:53 +0800

    新增使用tika取得pdf文件內容。

---
+++ src/example/LoadPdf.java
@@ -0,0 +1,33 @@
+package example;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.tika.parser.pdf.PDFParser;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.sax.BodyContentHandler;
+import org.xml.sax.ContentHandler;
+
+public class LoadPdf {
+	public static void main(String[] args) throws TikaException, IOException {
+
+		InputStream is = null;
+		try {
+			is = new FileInputStream("doc/esslli07_slides01.pdf");
+			ContentHandler contenthandler = new BodyContentHandler();
+			Metadata metadata = new Metadata();
+			PDFParser pdfparser = new PDFParser();
+			pdfparser.parse(is, contenthandler, metadata, new ParseContext());
+			System.out.println(contenthandler.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (is != null)
+				is.close();
+		}
+	}
+
+}
