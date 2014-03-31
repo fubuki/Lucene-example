3f4606f91bd0c7575cf3096da3887d26fb100592
fubuki <fubuki@users.noreply.github.com>
2014-03-31 23:48:13 +0800

    新增cjktokenizer的測試。

---
+++ src/example/Cjktokenizer.java
@@ -0,0 +1,39 @@
+package example;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.cjk.CJKAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.util.Version;
+
+public class Cjktokenizer {
+	public static void main(String[] args) throws IOException, ParseException {
+
+
+		Reader reader = new StringReader("中文維基百科將自己定位為一個包含人類所有知識領域的百科全書，而不是一本字典、詞典、論壇或任何其他性質的網站");
+			
+        Analyzer a = new CJKAnalyzer(Version.LUCENE_46);               
+        TokenStream ts = a.tokenStream("", reader);   
+        OffsetAttribute offsetAttribute = ts.getAttribute(OffsetAttribute.class);  
+        CharTermAttribute termAttribute = ts.getAttribute(CharTermAttribute.class);           
+        int n = 0;   
+        ts.reset();
+        while (ts.incrementToken()) {   
+            int startOffset = offsetAttribute.startOffset();  
+            int endOffset = offsetAttribute.endOffset();  
+            String term = termAttribute.toString();  
+            n++;   
+            System.out.println("Token("+n+") 的內容為："+term);   
+        }   
+        System.out.println("==共有詞條"+n+"條=="); 
+        
+        ts.end();
+        ts.close();
+	}
+}
