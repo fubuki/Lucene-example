7c886d71b19c10bfa40b1dd0e3f245382bd8ad81
fubuki <fubuki@users.noreply.github.com>
2014-04-05 20:29:28 +0800

    新增索引日期跟數字

---
+++ src/example/FieldIndex.java
@@ -0,0 +1,56 @@
+package example;
+
+import java.io.IOException;
+import java.util.Date;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.DateTools;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.util.Version;
+
+
+
+
+public class FieldIndex {
+	public static void main(String[] args) throws IOException {
+		
+		Directory directory = new RAMDirectory();
+		Document doc = new Document(); 
+		Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_46);
+		IndexWriter writer = new IndexWriter(directory, new IndexWriterConfig(Version.LUCENE_46, analyzer)) ; 
+		//DateTools
+		//Indexing dates & times
+		doc.add(new Field("Date", 
+				DateTools.dateToString(new Date(), DateTools.Resolution.DAY),
+				Field.Store.YES, 
+				Field.Index.NOT_ANALYZED)); 
+		
+		System.out.println("Date:"+doc.get("Date"));
+		
+		//Indexing Number
+		doc.add(new Field("size", "4096", Field.Store.YES, Field.Index.NOT_ANALYZED)); 
+		System.out.println("size:"+doc.get("size"));
+		doc.add(new Field("price", "10.99", Field.Store.YES, Field.Index.NOT_ANALYZED)); 
+		System.out.println("price:"+doc.get("price"));
+		doc.add(new Field("author", "Arthur C. Clark", Field.Store.YES, Field.Index.NOT_ANALYZED)); 
+		System.out.println("author:"+doc.get("author"));
+		
+		String lowerDomain = getSenderDomain().toLowerCase();
+		
+		if (isImportant(lowerDomain)) { 
+			doc.setBoost(1.5F); //1 
+		} else if (isUnimportant(lowerDomain)) { 
+			doc.setBoost(0.1F); //2 
+		} 
+
+		
+		writer.addDocument(doc);
+		
+	}
+}
