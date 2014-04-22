9fa36fff23ce1d9f91743aebed6e7beb4d344bfa
fubuki <fubuki@users.noreply.github.com>
2014-04-22 22:55:37 +0800

    測試 tika txtparser

--- src/example/TikaLoadFile.java
+++ src/example/TikaLoadFile.java
@@ -7,8 +7,11 @@ import java.io.InputStream;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.language.LanguageIdentifier;
 import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
 import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
 import org.apache.tika.parser.microsoft.OfficeParser;
+import org.apache.tika.parser.txt.TXTParser;
 import org.apache.tika.sax.BodyContentHandler;
 import org.xml.sax.ContentHandler;
 
@@ -16,6 +19,7 @@ public class TikaLoadFile {
 	public static void main(String[] args) throws TikaException, IOException {
 
 		InputStream is = null;
+		InputStream txt = null;
 		try {
 			is = new FileInputStream("doc/example.doc");
 			ContentHandler contenthandler = new BodyContentHandler();
@@ -23,10 +27,18 @@ public class TikaLoadFile {
 			OfficeParser officeparser = new OfficeParser();
 			officeparser.parse(is, contenthandler, metadata, new ParseContext());
 			
+			txt = new FileInputStream("doc/dic.txt");
+			ContentHandler txtContent = new BodyContentHandler();
+			Metadata Txtmetadata = new Metadata();
+			TXTParser txtparser = new TXTParser();
+			txtparser.parse(is, txtContent, Txtmetadata, new ParseContext());
+			
+			
 			LanguageIdentifier identifier = new LanguageIdentifier("WikipediA. English The Free Encyclopedia 4 490 000+ articles");
 			String language = identifier.getLanguage();
 			System.out.println(language);
 			System.out.println(contenthandler.toString());
+			System.out.println(Txtmetadata+":"+txtContent);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
