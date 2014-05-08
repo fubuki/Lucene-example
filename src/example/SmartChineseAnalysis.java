efc3be09d19aba2dc1f36bb39836648a0d4c0192
fubuki <fubuki@users.noreply.github.com>
2014-05-08 22:26:56 +0800

    測試lucene內建的中文分詞器。

---
+++ src/example/SmartChineseAnalysis.java
@@ -0,0 +1,35 @@
+package example;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.cn.smart.SmartChineseAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.util.Version;
+
+public class SmartChineseAnalysis {
+	public static void main(String[] args) throws IOException, ParseException {
+
+		Analyzer analyzer = new SmartChineseAnalyzer(Version.LUCENE_46,true);
+		String str = "中文維基百科將自己定位為一個包含人類所有知識領域的百科全書，而不是一本字典、詞典、論壇或任何其他性質的網站";
+		List<String> result = new ArrayList<String>();
+		try{
+			 TokenStream tokenStream = analyzer.tokenStream("field", str);    
+			 CharTermAttribute term=tokenStream.addAttribute(CharTermAttribute.class);    
+			 tokenStream.reset();       
+			 while( tokenStream.incrementToken() ){        
+				 result.add( term.toString() );       
+			 }       
+			 tokenStream.end();       
+			 tokenStream.close();   
+		 } catch (IOException e) {    
+			 e.printStackTrace();   
+		 }
+		
+		System.out.println(result);
+	}
+}
