606a08755c17ee6eb24913882f9a55bc80172670
fubuki <fubuki@users.noreply.github.com>
2016-08-12 11:55:32 +0800

    upgrade lucene version to 6.1.0.

--- src/main/java/example/FieldIndex.java
+++ src/main/java/example/FieldIndex.java
@@ -9,6 +9,7 @@ import org.apache.lucene.document.DateTools;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StoredField;
+import org.apache.lucene.document.StringField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
@@ -26,7 +27,7 @@ public class FieldIndex {
 		//Indexing dates & times
 		
 		
-		// 5.2 使用StoredField TextField 其他種類的 Field 的類型
+		//使用StoredField TextField 其他種類的 Field 的類型
 		doc.add(new StoredField("Date", 
 				DateTools.dateToString(new Date(), DateTools.Resolution.DAY)
 				)); 
@@ -34,14 +35,13 @@ public class FieldIndex {
 		System.out.println("Date:"+doc.get("Date"));
 		
 		//Indexing Number
-		doc.add(new Field("size", "4096", Field.Store.YES, Field.Index.NOT_ANALYZED)); 
+		doc.add(new StringField("size", "4096", Field.Store.YES));
 		System.out.println("size:"+doc.get("size"));
-		doc.add(new Field("price", "10.99", Field.Store.YES, Field.Index.NOT_ANALYZED)); 
+		doc.add(new StringField("price", "10.99", Field.Store.YES));
 		System.out.println("price:"+doc.get("price"));
-		doc.add(new Field("author", "Arthur C. Clark", Field.Store.YES, Field.Index.NOT_ANALYZED)); 
+		doc.add(new StringField("author", "Arthur C. Clark", Field.Store.YES));
 		System.out.println("author:"+doc.get("author"));
 				
 		writer.addDocument(doc);
-		
 	}
 }
